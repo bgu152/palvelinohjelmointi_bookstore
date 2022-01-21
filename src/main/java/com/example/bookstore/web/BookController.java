06960c9b7e92d700b41627b4576d05f7c2d108a7
bgu152 <erikrammschmidt@gmail.com>
2022-01-21 14:25:17 +0200

    ch3 teht 3 valmis

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
+
 
 @Controller
 public class BookController {
@@ -20,9 +24,29 @@ public class BookController {
 		return "booklist";
 	}
 	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		brepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	//Add new book
+	@RequestMapping(value ="/add")
+	public String addStudent(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	//Save new book
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		brepository.save(book);
+		return "redirect:booklist";		
+	}
+	
 	@GetMapping("/index")
 	public String showBooks() {
-		return "index";		
+		return "index";
 	}
 
 }
